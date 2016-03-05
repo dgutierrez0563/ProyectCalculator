485fbadd80571bf2d08d4e9aac566a1b127b8dcc
David Gutierrez <dgutierrez06@gmail.com>
2016-03-05 02:40:16 -0600

    ProyectCalculator
    First Commit Proyect Calculator

---
+++ ProyectCalculator/src/proyectcalculator/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectcalculator;
+/**
+ * @author w.sullivan-DAVID GUTIERREZ SOLANO
+ * @version Calculator 1.0v
+ * @date 2016-03-02
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WindowStar instanciaWindowStar=new WindowStar();
+        instanciaWindowStar.setVisible(true);
+    }   
+}
