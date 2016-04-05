949db5dc55705a2408b788a844894bbdaed1cb5f
David Gutierrez <dgutierrez06@gmail.com>
2016-04-04 22:57:18 -0600

    Proyect Calculator 2.0v Final
    Version 2.0v

---
+++ Proyect Calculator 2.0v Final/ProyectCalculator/src/Main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import VentanaInicial.WindowStar;   //Se importa el paquete VentanaInicial.WindowStar para la ventana principal
+/**
+ * @author w.sullivan-DAVID GUTIERREZ SOLANO
+ * @version Calculator 2.0v
+ * @create 2016-03-02
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {    //Clase Main
+        // TODO code application logic here
+        WindowStar instanciaWindowStar=new WindowStar();    //Creacion del Object instanciaWindowStar para WindowsStar
+        instanciaWindowStar.setVisible(true);   //instanciaWindowStar vuelve true la visibilidad del Frame Principal
+    }   
+}
